95ea214e73ec95a6e88dcdd64bb5ac2ed7b3dee4
Tim Huegdon <tim@nefariousdesigns.co.uk>
2010-10-26 17:38:07 +0100

    Debugging update.

--- bot.js
+++ bot.js
@@ -58,7 +58,7 @@ client.addListener('message', function (from, to, message) {
                 client.say(config.channel, message);
                 updateLinks.push({
                     "url": result.url,
-                    "count": result.count++
+                    "count": parseInt(result.count)++
                 });
                 
                 console.log(util.inspect(updateLinks));
